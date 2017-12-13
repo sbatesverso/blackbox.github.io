d00f72a410232251abf0fd9449cd4a77182205d1
sbatesverso <34386785+sbatesverso@users.noreply.github.com>
2017-12-13 09:22:24 -0800

    Revert "PP-2"
    This reverts commit 3a56dddfb6e0dc7fd30a58b5fd1ea3182b7d59b9.

--- js/main.js
+++ js/main.js
@@ -7,46 +7,9 @@ if(!/iPhone/i.test(navigator.userAgent)) {
       $(e.relatedTarget).find('video').get(0).play();
     });
 
-    $('#vLifestyle').bind('slide.bs.carousel', function(e) {
+    $('#vLifestyle').bind('slid.bs.carousel', function(e) {
       $('video').not('.active > div > video').each(function() {
         $(this).get(0).pause();
       });
     });
   }
-
-
-(function() {
-
-	//Function to animate slider captions 
-	function doAnimations( elems ) {
-		//Cache the animationend event in a variable
-		var animEndEv = 'webkitAnimationEnd animationend';
-		
-		elems.each(function () {
-			var $this = $(this),
-				$animationType = $this.data('animation');
-			$this.addClass($animationType).one(animEndEv, function () {
-				$this.removeClass($animationType);
-			});
-		});
-	}
-	
-	//Variables on page load 
-	var $myCarousel = $('#vLifestyle'),
-		$firstAnimatingElems = $myCarousel.find('.item:first').find("[data-animation ^= 'animated']");
-		
-		
-	//Animate captions in first slide on page load 
-	doAnimations($firstAnimatingElems);
-	
-	//Pause carousel  
-	$myCarousel.carousel('pause');
-	
-	
-	//Other slides to be animated on carousel slide event 
-	$myCarousel.on('slide.bs.carousel', function (e) {
-		var $animatingElems = $(e.relatedTarget).find("[data-animation ^= 'animated']");
-		doAnimations($animatingElems);
-	});  
-	
-})(jQuery);
